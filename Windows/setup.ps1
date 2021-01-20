8879ec2a62cea962dd94da8b8843fbfcbccd5d64
KMConner <KMConner@users.noreply.github.com>
2021-01-21 01:14:14 +0900

    Add setup files

---
+++ Windows/setup.ps1
@@ -0,0 +1,7 @@
+# TODO: install chocolatey
+
+choco install -y firefox git jetbrainstoolbox adobereader gpg4win slack keybase `
+    docker-desktop golang zoom javaruntime haskell-stack cabal nodejs teraterm yarn youtube-dl `
+    crystaldiskinfo.install crystaldiskmark ffmpeg vlc make itunes discord wireshark
+
+# Install Visual Studio, Addons for Firefox, Visual Studio Code, Office
